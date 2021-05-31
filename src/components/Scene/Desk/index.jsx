4c5356b6fe7049595870a137c34a4092160d925e
Dustin Craig <dustincraig@Dustins-MBP.attlocal.net>
2021-05-30 22:29:54 -0400

    Finished desk

--- src/components/Scene/Desk/index.jsx
+++ src/components/Scene/Desk/index.jsx
@@ -7,9 +7,41 @@ const Desk = () => (
         </mesh>
 
         {/* Legs */}
+
+        {/* Front Left */}
         <mesh
             scale={[0.3, 4, 0.3]}
             position={[-4.6, -2, 1]}
+            rotation={[0, 20, 0]}
+        >
+            <boxGeometry />
+            <meshPhongMaterial />
+        </mesh>
+
+        {/* Front Right */}
+        <mesh
+            scale={[0.3, 4, 0.3]}
+            position={[2.1, -1.9, 3.9]}
+            rotation={[0, 20, 0]}
+        >
+            <boxGeometry />
+            <meshPhongMaterial />
+        </mesh>
+
+        {/* Back Left */}
+        <mesh
+            scale={[0.3, 4, 0.3]}
+            position={[4.5, -2.1, -1.9]}
+            rotation={[0, 20, 0]}
+        >
+            <boxGeometry />
+            <meshPhongMaterial />
+        </mesh>
+
+        {/* Back Right */}
+        <mesh
+            scale={[0.3, 4, 0.3]}
+            position={[-2, -2.1, -4.5]}
             rotation={[0, 20, 0]}
         >
             <boxGeometry />
